08b78c0ca01131fb2706dd2bb19148cbe281489d
Comet1995 <37407810+Comet1995@users.noreply.github.com>
2023-12-06 10:47:50 +0800

    1

--- 5-运算符.py
+++ 5-运算符.py
@@ -2,7 +2,7 @@
 赋值运算符 = += -= *= /= //= **/
 算数运算符： + - * / % // **
 关系运算符：> < >= <= == != is 
-逻辑运算符：
+逻辑运算符：and or not
 '''
 '''a = 1
 b = 2
@@ -16,10 +16,32 @@ print(a,b,c)'''
 键盘输入一个3位数的整数，打印个位数，十位数，百位数
 '''
 
-'''num = input('请输入一个三位数')
+'''
+num = input('请输入一个三位数')
 baiwei = int(num)//100
 print(baiwei)
 shiwei = int(num)%100//10
 print(shiwei)
 gewei = int(num)%10
-print(gewei)'''
+print(gewei)
+'''
+
+'''
+and:与，并且 只要有一侧为假，结果即为假
+A and B
+Ture and True--->True
+Ture and False--->False
+False and True--->False
+False and False--->False
+两侧都为真时，输出后边的值
+'''
+
+'''
+or: 或，或者 只要有一侧为真，结果即为真
+A and B
+Ture and True--->True
+Ture and False--->True
+False and True--->True
+False and False--->False
+输出第一个真值
+'''
